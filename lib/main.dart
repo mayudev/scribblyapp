74b91332714901d65d8f0ce3eaf0684abd4b8b6c
mayudev <99561108+mayudev@users.noreply.github.com>
2022-08-04 14:27:01 +0200

    feat: theme tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scribbly/pages/home.dart';
+import 'package:scribbly/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: buildTheme(),
       home: const HomePage(),
     );
   }
---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+ThemeData buildTheme() {
+  const primary = Color(0xff684b77);
+
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: primary));
+
+  var base = ThemeData();
+
+  return base.copyWith(
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light().copyWith(
+      primary: primary,
+      secondary: Colors.red[200]!,
+    ),
+    primaryColor: primary,
+    appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: primary,
+        titleTextStyle:
+            const TextStyle(fontWeight: FontWeight.w400, fontSize: 20.0)),
+  );
+}
