8072ffd97270d52f832d9342de2eca31cc29ca8b
mayudev <99561108+mayudev@users.noreply.github.com>
2022-08-04 15:06:09 +0200

    feat: add details

--- lib/widgets/details.dart
+++ lib/widgets/details.dart
@@ -10,40 +10,54 @@ class Details extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Card(
-            margin: cardMargin,
-            child: Column(
-              children: [
-                Image.network(
-                  data.details.coverUrl,
-                  height: 220.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data.details.title,
-                        style: const TextStyle(
-                          fontSize: 22.0,
+    return Scrollbar(
+      thumbVisibility: true,
+      interactive: true,
+      thickness: 10.0,
+      radius: const Radius.circular(8.0),
+      child: ListView(
+        children: [
+          Card(
+              margin: cardMargin,
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      data.details.coverUrl,
+                      height: 220.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data.details.title,
+                          style: const TextStyle(
+                            fontSize: 22.0,
+                          ),
                         ),
-                      ),
-                      Text(data.details.author.username,
+                        Text(
+                          data.details.author.username,
                           style: const TextStyle(
                             fontSize: 14.0,
                             fontWeight: FontWeight.w300,
-                          ))
-                    ],
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Start reading'))
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            )),
-        _synopsisCard(),
-        _chaptersCard(),
-      ],
+                ],
+              )),
+          _synopsisCard(),
+          _chaptersCard(),
+        ],
+      ),
     );
   }
 
@@ -51,6 +65,7 @@ class Details extends StatelessWidget {
     return Card(
       margin: cardMargin,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _cardHeading(Icons.info, 'Synopsis'),
           Padding(
@@ -60,7 +75,58 @@ class Details extends StatelessWidget {
               style: const TextStyle(height: 1.5),
             ),
           ),
+          _cardHeading(Icons.style, 'Genres'),
+          _chips(
+            child: Row(
+              children: _genreChips(),
+            ),
+          ),
+          _cardHeading(Icons.local_offer, 'Tags'),
+          _chips(
+            child: Row(
+              children: _tagChips(),
+            ),
+          )
         ],
+      ),
+    );
+  }
+
+  Widget _chips({required Widget child}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: child,
+      ),
+    );
+  }
+
+  List<Widget> _genreChips() {
+    return data.details.genres
+        .map((genre) => _genreChip(
+              label: Text(genre),
+            ))
+        .toList();
+  }
+
+  List<Widget> _tagChips() {
+    return data.details.tags
+        .map((genre) => _genreChip(
+              label: Text(genre),
+            ))
+        .toList();
+  }
+
+  Widget _genreChip({label = Widget}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 2.0),
+      child: Chip(
+        label: label,
+        padding: const EdgeInsets.all(2.0),
+        labelStyle: const TextStyle(
+          fontSize: 13.0,
+        ),
       ),
     );
   }
