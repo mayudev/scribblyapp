7cef8a50d7c3d86fcbb9689389e7259b7c8cd6d3
mayudev <99561108+mayudev@users.noreply.github.com>
2022-08-04 23:30:23 +0200

    feat: add bottom navigation bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:scribbly/pages/novel.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with RestorationMixin {
+  final RestorableInt _index = RestorableInt(0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('ScribblyApp')),
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              _openNovelPage(context, "Test Novel", 310493);
-            },
-            child: const Text('example novel page')),
+      appBar: AppBar(
+        title: const Text('ScribblyApp'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+            tooltip: 'Search',
+          )
+        ],
       ),
+      body: IndexedStack(
+        index: _index.value,
+        children: _pages(),
+      ),
+      bottomNavigationBar: _buildBottomNavigation(),
     );
+  }
+
+  List<Widget> _pages() {
+    return [
+      Center(
+        child: TextButton(
+          onPressed: () {
+            _openNovelPage(context, "Test Novel", 310493);
+          },
+          child: const Text('open'),
+        ),
+      ),
+      const Text('page 2'),
+    ];
+  }
+
+  Widget _buildBottomNavigation() {
+    return BottomNavigationBar(
+        currentIndex: _index.value,
+        onTap: (i) => setState(() => _index.value = i),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.collections_bookmark),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Discover',
+          ),
+        ]);
   }
 
   void _openNovelPage(BuildContext context, String title, int novelId) {
@@ -25,5 +69,13 @@ class HomePage extends StatelessWidget {
         builder: (context) => NovelPage(title: title, id: novelId),
       ),
     );
+  }
+
+  @override
+  String get restorationId => 'home_page';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_index, 'nav_bar_index');
   }
 }
