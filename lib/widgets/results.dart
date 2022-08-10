2d0711ecb9563daf7eb5adc7cf1dbad15790f731
mayudev <99561108+mayudev@users.noreply.github.com>
2022-08-11 00:08:14 +0200

    fix: remove unused imports

--- lib/widgets/results.dart
+++ lib/widgets/results.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:scribbly/types/novel.dart';
 import 'package:scribbly/widgets/error_screen.dart';
 import 'package:scribbly/widgets/result_card.dart';
